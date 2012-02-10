6072b3c95a73ecedeb3e9c5c4f5f223c309caf41
apex <apex@f543b97e-dff5-4858-a075-c57f815778c8>
2012-02-10 21:01:11 +0000

    post auto save bug fix #1 quick edit works better now. -Apex

--- includes/layout/quickedit.php
+++ includes/layout/quickedit.php
@@ -1,8 +1,10 @@
 <div class="new-post">
 Title:<br/><textarea id="post_title-<?=$post['id']?>"><?=$post['title']?></textarea>
 <br/>Content:<br/>
-<textarea id="post_content-<?=$post['id']?>"><?=$post['content']?></textarea>
-<button onclick="xajax_quickEditPost('<?=$post['id']?>',mynicEditor<?=$post['id']?>.instanceById('post_content-<?=$post['id']?>').getContent(),document.getElementById('post_title-<?=$post['id']?>').value)">save post</button>
+<textarea id="post_content"><?=$post['content']?></textarea>
+<button 
+onclick="xajax_quickEditPost('<?=$post['id']?>',mynicEditornew.instanceById('post_content').getContent(),document.getElementById('post_title-<?=$post['id']?>').value)">save post</button>
 <button onclick="xajax_cancel_addPost('<?=$post['id']?>')">cancel</button>
-<button onclick="xajax_updatePost('<?=$post['id']?>','inquick',mynicEditor<?=$post['id']?>.instanceById('post_content-<?=$post['id']?>').getContent())">full editor</button>
+<button 
+onclick="xajax_updatePost('<?=$post['id']?>','inquick',mynicEditornew.instanceById('post_content').getContent())">full editor</button>
 </div>
