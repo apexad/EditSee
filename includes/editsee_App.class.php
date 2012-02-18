bf5505fb6746964d4ecc41143ea420e45500137a
apex <apex@f543b97e-dff5-4858-a075-c57f815778c8>
2012-02-18 19:23:36 +0000

    rolls back the auto updare editsee path.  Ran into some bugs. Creating a Trac ticket.  We can revisit this later. -Apex

--- includes/editsee_App.class.php
+++ includes/editsee_App.class.php
@@ -28,7 +28,8 @@ class editsee_App {
 		if ($this->configFileExists() !== false) {
 			$this->config_file = $this->configFileExists();
 			//load the database, populate $this->page
-			require_once($_SERVER['DOCUMENT_ROOT'].str_replace('index.php','',$_SERVER['REQUEST_URI']).$this->config_file);
+			require_once($_SERVER['DOCUMENT_ROOT'].'/'.$this->config_file);
+			//require_once($_SERVER['DOCUMENT_ROOT'].str_replace('index.php','',$_SERVER['REQUEST_URI']).$this->config_file);
 			$this->db = new editsee_Database($type, trim($host), trim($user), trim($password), trim($database),trim($table_prefix));
 			$this->title = $this->get_config('es_title');
 			$this->header = "\n".'<link rel="alternate" type="application/rss+xml" title="'.$this->get_config('es_title').' &raquo; Feed" href="'.$this->get_config('es_main_url').'feed/" />';
@@ -69,7 +70,7 @@ class editsee_App {
 
 			if ($this->loggedIn()) {
 				//auto-detect and fix main_url
-				$this->update_config('es_main_url', 'http://'.$_SERVER['HTTP_HOST'].str_replace('index.php','',$_SERVER['REQUEST_URI']));
+				//$this->update_config('es_main_url', 'http://'.$_SERVER['HTTP_HOST'].str_replace('index.php','',$_SERVER['REQUEST_URI']));
 				$this->header .= "\n".'<link rel="stylesheet" type="text/css" href="'.$this->get_config('es_main_url').'includes/layout/topbar.css" />';
 				$this->header .= "\n".'<link rel="stylesheet" type="text/css" href="'.$this->get_config('es_main_url').'includes/layout/loggedin.css" />';
 			}
@@ -88,7 +89,8 @@ class editsee_App {
 	
 	public static function configFileExists() {
 		//check for a config file
-		$handler = opendir($_SERVER['DOCUMENT_ROOT'].str_replace('index.php','',$_SERVER['REQUEST_URI']));
+		//$handler = opendir($_SERVER['DOCUMENT_ROOT'].str_replace('index.php','',$_SERVER['REQUEST_URI']));
+		$handler = opendir($_SERVER['DOCUMENT_ROOT'].'/');
 
 		while ((false !== ($file = readdir($handler)))) {
 			if (preg_match('/\w*config\w*\.php/',$file) == 1)
